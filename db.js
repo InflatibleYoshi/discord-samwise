f83f65bf0124267391f4d1d9dc09c0ae1c8fa980
Daniel Ananth <dananth@protonmail.com>
2020-04-29 20:46:50 -0400

    7 Finally successfully implemented promise.
    I think i've had enough of javascript but i've finally learned Promises to a basic degree.

--- db.js
+++ db.js
@@ -1,175 +1,180 @@
-const db = require('gun')({
-    file: 'database.json'
-});
+const Gun = require( "gun" );
+require('gun/lib/then.js')
+require('gun/lib/unset.js')
 const USERS = 'users';
 const REQUEST_LIST = 'request_list';
 const INVITE_LIST = 'invite_list';
-const USER_FELLOWSHIPS = "user_fellowships"
+const FELLOWSHIP_MEMBER_OF = "fellowship_member_of"
 const FELLOWSHIP = 'fellowship';
 
-// All these functions return strings
-
-function getUser(id) {
-    let user = db.get(USERS).get(id).not(() => {
-        throw new Error("Specified user %s does not exist.")
-    });
-}
-
-function addUser(id, streak, isTracking) {
-    db.get(USERS).get(id).not(function (id) {
-        let user = db.get(id).put({
-            streak: streak,
-            isTracking: isTracking,
-        });
-        db.get(USERS).set(user);
-        return "User added to database."
-    });
-    return "User already exists in database."
-}
-
-function deleteUser(id) {
-    try {
-        getUser(id).put(null);
-    } catch (e) {
-        return e
+class database {
+    constructor(){
+        this.db = new Gun();
     }
-    return "User %s was deleted."
-}
 
-function requestToJoinFellowship(requestingId, targetId) {
-    try {
-        let requestingUser = getUser(requestingId);
-        let targetUser = getUser(targetId);
-        targetUser.get(REQUEST_LIST).set(requestingUser);
-    } catch (e) {
-        return e
+    getUser(id) {
+        if(this.db.get(USERS).is(id)){
+            return this.db.get(USERS).get(id).promise();
+        }
     }
-    return "Request sent to user %s"
-}
 
-function inviteToJoinFellowship(invitingId, targetId) {
-    try {
-        let invitingUser = getUser(invitingId);
-        let targetUser = getUser(targetId);
-        targetUser.get(INVITE_LIST).set(invitingUser);
-    } catch (e) {
-        return e
+    addUser(id, streak, isTracking, successHandler, failureHandler) {
+        let db = this.db;
+        db.get(USERS).get(id).promise((resolved) => {
+            if(resolved.put === undefined){
+                let user = db.get(id).put({
+                    streak: streak,
+                    streak_max: streak,
+                    isTracking: isTracking,
+                    fellowship: {},
+                    fellowship_member_of: {},
+                    request_list: {},
+                    invite_list: {},
+                });
+                db.get(USERS).set(user);
+            } else {
+                throw "User already exists in database."
+            }
+            return streak
+
+        }).then(successHandler, failureHandler);
     }
-    return "Invite sent to user %s"
-}
 
-function answerRequest(requestingId, targetId, isAccept) {
-    try {
-        let requestingUser = getUser(requestingId);
-        let targetUser = getUser(targetId);
-        targetUser.get(REQUEST_LIST).get(requestingUser)
-            .not(() => {throw new Error("Request does not exist.")})
-            .once(function (targetUser, requestingUser) {
-                targetUser.get(REQUEST_LIST).unset(requestingUser);
-                if (isAccept) {
-                    targetUser.get(FELLOWSHIP).set(requestingUser);
-                    requestingUser.get(USER_FELLOWSHIPS).set(targetUser);
-                }
+    requestToJoinFellowship(requestingId, targetId) {
+        try {
+            let requestingUser = this.getUser(requestingId);
+            let targetUser = this.getUser(targetId);
+            targetUser.get(REQUEST_LIST).set(requestingUser);
+        } catch (e) {
+            throw e
+        }
+        return "Request sent to join %s's fellowship."
+    }
+
+    inviteToJoinFellowship(invitingId, targetId) {
+        try {
+            let invitingUser = this.getUser(invitingId);
+            let targetUser = this.getUser(targetId);
+            targetUser.get(INVITE_LIST).set(invitingUser);
+        } catch (e) {
+            return e
+        }
+        return "Invite sent to user %s"
+    }
+
+    answerRequest(requestingId, targetId, isAccept) {
+        try {
+            let requestingUser = this.getUser(requestingId);
+            let targetUser = this.getUser(targetId);
+            targetUser.get(REQUEST_LIST).get(requestingId)
+                .not(() => {throw new Error("Request does not exist.")})
+                .once(function () {
+                    targetUser.get(REQUEST_LIST).unset(requestingUser);
+                    if (isAccept) {
+                        targetUser.get(FELLOWSHIP).set(requestingUser);
+                        requestingUser.get(FELLOWSHIP_MEMBER_OF).set(targetUser);
+                    }
+                });
+        } catch (e) {
+            return e
+        }
+        if (isAccept) {
+            return "You have accepted %s into your fellowship."
+        } else {
+            return "You have rejected %s from your fellowship"
+        }
+    }
+
+    answerInvite(invitingId, targetId, isAccept) {
+        try {
+            let invitingUser = this.getUser(invitingId);
+            let targetUser = this.getUser(targetId);
+            targetUser.get(INVITE_LIST).get(invitingId)
+                .not(() => {throw new Error("Invite does not exist.")})
+                .once(function(){
+                    targetUser.get(INVITE_LIST).unset(invitingUser);
+                    if (isAccept) {
+                        invitingUser.get(FELLOWSHIP).set(targetUser);
+                        targetUser.get(FELLOWSHIP_MEMBER_OF).set(invitingUser)
+                    }
+                });
+        } catch (e) {
+            return e
+        }
+        if (isAccept) {
+            return "You have accepted %s's invitation."
+        } else {
+            return "You have rejected %s's invitation."
+        }
+    }
+
+    kickFromFellowship(kickedId, targetId) {
+        try {
+            let kickedUser = this.getUser(kickedId);
+            let targetUser = this.getUser(targetId);
+            targetUser.get(FELLOWSHIP).get(kickedId)
+                .not(() => { throw new Error("User %s is not in your fellowship.")})
+                .once(function () {
+                    targetUser.get(FELLOWSHIP).unset(kickedUser);
+                    kickedUser.get(FELLOWSHIP_MEMBER_OF).unset(targetUser);
+                });
+        } catch (e) {
+            return e
+        }
+        return "You have kicked %s from your fellowship."
+    }
+
+    leaveFellowship(leavingId, targetId) {
+        try{
+            let leavingUser = this.getUser(leavingId);
+            let targetUser = this.getUser(targetId);
+            targetUser.get(FELLOWSHIP).get(leavingId)
+                .not(() => {throw new Error("You are not in %s's fellowship")})
+                .once(function () {
+                    targetUser.get(FELLOWSHIP).unset(leavingUser);
+                    leavingUser.get(FELLOWSHIP_MEMBER_OF).unset(targetUser);
+                });
+        } catch (e) {
+            return e
+        }
+        return "You have left %s's fellowship."
+    }
+
+    reset(id){
+        try{
+            this.getUser(id).put({
+                streak: 0,
             });
-    } catch (e) {
-        return e
+        } catch(e){
+            return e
+        }
     }
-    if (isAccept) {
-        return "You have accepted %s into your fellowship."
-    } else {
-        return "You have rejected %s from your fellowship"
+
+    listAllUsers() {
+        let users = [];
+        this.db.get(USERS).map().once(v => users.push(v));
+        return users;
     }
-}
 
-function answerInvite(invitingId, targetId, isAccept) {
-    try {
-        let invitingUser = getUser(invitingId);
-        let targetUser = getUser(targetId);
-        targetUser.get(INVITE_LIST).get(invitingUser)
-            .not(() => {throw new Error("Invite does not exist.")})
-            .once(function(INVITE_LIST, invitingUser){
-                targetUser.get(INVITE_LIST).unset(invitingUser);
-                if (isAccept) {
-                    invitingUser.get(FELLOWSHIP).set(targetUser);
-                    targetUser.get(USER_FELLOWSHIPS).set(invitingUser)
-                }
-            });
-    } catch (e) {
-        return e
+    listRequests(id) {
+        let users = [];
+        this.db.get(id).get(REQUEST_LIST).map().once(v => users.push(v));
+        return users;
     }
-    if (isAccept) {
-        return "You have accepted %s's invitation."
-    } else {
-        return "You have rejected %s's invitation."
+
+    listInvites(id) {
+        let users = [];
+        this.db.get(id).get(INVITE_LIST).map().once(v => users.push(v));
+        return users;
     }
-}
 
-function kickFromFellowship(kickedId, targetId) {
-    try {
-        let kickedUser = getUser(kickedId);
-        let targetUser = getUser(targetId);
-        targetUser.get(FELLOWSHIP).get(kickedUser)
-            .not(() => { throw new Error("User %s is not in your fellowship.")})
-            .once(function (FELLOWSHIP, kickedUser, targetUser) {
-                targetUser.get(FELLOWSHIP).unset(kickedUser);
-                kickedUser.get(USER_FELLOWSHIPS).unset(targetUser);
-            });
-    } catch (e) {
-        return e
+    listFellowship(id) {
+        let users = [];
+        this.db.get(id).get(FELLOWSHIP).map().once(v => users.push(v));
+        return users;
     }
-    return "You have kicked %s from your fellowship."
+
+
 }
 
-function leaveFellowship(leaveId, targetId) {
-    try{
-        let leavingUser = getUser(leaveId);
-        let targetUser = getUser(targetId);
-        targetUser.get(FELLOWSHIP).get(leavingUser)
-            .not(() => {throw new Error("You are not in %s's fellowship")})
-            .once(function (FELLOWSHIP, leavingUser) {
-                targetUser.get(FELLOWSHIP).unset(leavingUser);
-                leavingUser.get(USER_FELLOWSHIPS).unset(leavingUser);
-        });
-    } catch (e) {
-        return e
-    }
-    return "You have left %s's fellowship."
-}
-
-function reset(id){
-    try{
-        getUser(id).put({
-            streak: 0,
-        });
-    } catch(e){
-        return e
-    }
-}
-
-function listAllUsers() {
-    let users = [];
-    db.get(USERS).map().once(v => users.push(v));
-    return users;
-}
-
-function listRequests(id) {
-    let users = [];
-    db.get(id).get(REQUEST_LIST).map().once(v => users.push(v));
-    return users;
-}
-
-function listInvites(id) {
-    let users = [];
-    db.get(id).get(INVITE_LIST).map().once(v => users.push(v));
-    return users;
-}
-
-function listFellowship(id) {
-    let users = [];
-    db.get(id).get(FELLOWSHIP).map().once(v => users.push(v));
-    return users;
-}
-
-module.exports = { addUser, deleteUser, requestToJoinFellowship, inviteToJoinFellowship,
-         answerRequest, answerInvite, kickFromFellowship, leaveFellowship,
-         reset, listAllUsers, listRequests, listInvites, listFellowship }
+module.exports = { database }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Eris = require('eris');
+const Chrono = require('chrono-node');
 const db = require('./db.js');
 const {token} = require('./variables');
 const bot = new Eris.CommandClient(token, {}, {
@@ -6,40 +7,70 @@ const bot = new Eris.CommandClient(token, {}, {
     owner: "InflatibleYoshi",
     prefix: "!"
 });
+let dbConnection;
 
 bot.on("ready", () => {
+    dbConnection = new db.database();
     console.log("Ready!");
 });
 
-bot.registerCommand("register", (msg, args) => {
-        console.log("Register User.");
-        if (args.length < 2 || args.length > 2) {
-            return "Invalid input";
-        }
-        let streak = Number(args[0]);
-        let isMentor = Boolean(args[1]);
-        db.addUser(msg.author.id);
-        bot.createMessage(msg.channel.id, `Your user has been created!`);
+bot.on("messageReturn", (msg, msgToReturn) => {
+    bot.createMessage(msg.channel.id, msgToReturn);
+})
 
+const registration = bot.registerCommand("register", () => {
+        return "In order to register, type '!register silent' to register without tracking your clean days." +
+            "\nIf you would like to track your clean days, type '!register date \"timestamp\" filling" +
+            "\n\"timestamp\" with the last day you partook in your 'habit' in the format: " +
+            "\n'DD Mon YYYY HH:mm:ss TZ' (01 Jan 1970 00:00:00 GMT).\""
     },
     {
         description: "Register User",
         fullDescription: "Register user to the Samwise bot.",
-    })
-    .registerSubcommand("silent", (msg) => "0 false",
-        {
-            description: "Register user - silent",
-            fullDescription: "Register user to the Samwise bot without reporting.",
-        }
-    )
-    .registerSubcommand("date", (msg, args) => {
+    });
+
+registration.registerSubcommand("silent", (msg) => {
+        console.log("silent");
+        dbConnection.addUser(msg.author.id, 0, false);
+        },
+    {
+        description: "Register user - silent",
+        fullDescription: "Register user to the Samwise bot without reporting.",
+    }
+);
+
+registration.registerSubcommand("date", (msg, args) => {
+        console.log("date");
         const text = args.join(" "); // Make a string of the text after the command label
-        let streak = Math.abs(Date.now() - new Date.parse(text)) / 86400000;
-        bot.createMessage(msg.channel.id, `This puts your current streak at ${streak} days.`);
-        return `${streak} true`
-    },{
-        description: "Set date",
+        console.log(text);
+        const results = Chrono.parse(text);
+        const streak = Math.floor((results[0].ref.getTime() - results[0].start.date().getTime()) / (1000 * 3600 * 24));
+        console.log(streak);
+
+        let successHandler = function(value) {
+            bot.emit("messageReturn", msg, "This puts your current streak at " + value + " clean days.\n" +
+                "Invite people to join your fellowship or ask people to join theirs!")
+        }
+
+        let failureHandler = function(reason) {
+            bot.emit("messageReturn", msg, reason);
+        }
+        dbConnection.addUser(msg.author.id, streak, true, successHandler, failureHandler);
+
+    }, {
+        description: "Register user - tracking",
         fullDescription: "Fill in the space to the right of date with the last day you partook in your 'habit'" +
             " in the format 'DD Mon YYYY HH:mm:ss TZ' (01 Jan 1970 00:00:00 GMT)."
+    }
+);
+
+bot.registerCommand("unregister", (msg) => {
+        dbConnection.deleteUser(msg.author.id);
+        return "Your user has been unregistered."
+    },
+    {
+        description: "Unregister User",
+        fullDescription: "Unregister user from the Samwise bot.",
     });
+
 bot.connect();
